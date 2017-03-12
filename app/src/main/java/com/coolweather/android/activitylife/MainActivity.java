6ecf10dbc37a3f54be44bb7cbc67596306325832
guolifeng <glf_work@163.com>
2017-03-12 22:19:26 +0800

    2:资源相关的系统配置发生改变导致activity被杀死并重新创建的生命周期

--- app/src/main/java/com/coolweather/android/activitylife/MainActivity.java
+++ app/src/main/java/com/coolweather/android/activitylife/MainActivity.java
@@ -3,6 +3,7 @@ package com.coolweather.android.activitylife;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity
@@ -13,6 +14,12 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null)
+        {
+
+            String test = savedInstanceState.getString("test");
+            Log.i(TAG, "savedInstanceState: " + test);
+        }
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate: ");
     }
@@ -57,6 +64,21 @@ public class MainActivity extends Activity
     {
         super.onPause();
         Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putString("test", ((EditText) this.findViewById(R.id.text)).getText().toString());
+        Log.i(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState: ");
     }
 }
 
